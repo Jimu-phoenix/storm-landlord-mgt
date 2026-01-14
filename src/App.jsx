691b2ecccd19d1c1d6a1d22d6c4a1fcefb118e00
PreciousNamondwe <bis22-pnamondwe@mubas.ac.mw>
2026-01-14 17:56:48 +0000

    initial commit of features dashboard frame implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Frame from './components/Frame'
 import './App.css'
 
 function App() {
 
   return (
-    <></>
+    <>
+    <Frame/>
+    </>
   )
 }
 
